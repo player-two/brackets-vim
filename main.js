f6108639e64fb270d1dbc04ec134cc61e7ead118
megalord <jwgriege@sbcglobal.net>
2014-05-13 00:59:35 -0500

    Added support for tab autocompletion in e and vsp commands.

--- main.js
+++ main.js
@@ -21,14 +21,57 @@ define(function (require, exports, module) {
         fileMenu    = Menus.getMenu(Menus.AppMenuBar.FILE_MENU),
         isEnabled   = true,
         isSplit     = false,
-        rootPath    = "";
+        projectRoot    = "";
 
     /*
     TODO:
     - keybind file switching to Ctrl-w
     - add vim's mode to status bar
     - fix bug where initial editor shows file that is not in the working set
+    - consider leaving working set untouched, using :nth-child(n+1) to hide other files
     */
+
+    // Extend the vim command dialog to autocomplete filepaths with the tab key.
+    function handleTabKey(jqEvent) {
+        var args, dirs, inputElement, partialName, relativePath;
+        if (event.keyCode === 9) {
+            // Check if the vim dialog is focused.
+            inputElement = document.activeElement;
+            if(inputElement.parentElement.classList.contains("CodeMirror-dialog-bottom")) {
+                // Prevent a focus change to the next input in the DOM.
+                jqEvent.preventDefault();
+
+                args = jqEvent.target.value.split(" ");
+                if (args.length !== 2 || (args[0] !== "e" && args[0] !== "vsp")) {
+                    return;
+                }
+
+                dirs = args[1].split("/");
+                relativePath = dirs.slice(0, -1).join("/");
+                if (relativePath.length > 0) {
+                    relativePath += "/";
+                }
+                partialName = dirs.slice(-1).join("/");
+
+                FileSystem.resolve(projectRoot + relativePath, function (notUsed, Directory) {
+                    var match = "";
+                    Directory._contents.forEach(function (item) {
+                        if (item._name.indexOf(partialName) !== -1) {
+                            match = item._name;
+                        }
+
+                        if (item.hasOwnProperty("_isDirectory") && item._isDirectory) {
+                            match += "/";
+                        }
+                    });
+
+                    if (match.length > 0) {
+                        inputElement.value = args[0] + " " + relativePath + match;
+                    }
+                });
+            }
+        }
+    }
 
     // Patch the esc key, which does not properly exit insert or visual mode in Brackets.
     // This is likely due to Brackets, since the demo at codemirror.net/demo/vim.html works fine.
@@ -52,6 +95,14 @@ define(function (require, exports, module) {
         $editorHolder.addClass("split-view");
     }
 
+    // Toggles vim functionality in the editor.
+    // This function is run by the menu item.
+    function toggleVim(editor) {
+        isEnabled = !isEnabled;
+        CommandManager.get(commandId).setChecked(isEnabled);
+        updateEditorMode();
+    }
+
     // Revert the UI back to a single-document view.
     function unsplit() {
         isSplit = false;
@@ -73,20 +124,16 @@ define(function (require, exports, module) {
             cm.setOption("vimMode", isEnabled);
 
             $(activeEditor).off("keyEvent", handleEscKey);
+            $(document).off("keydown", handleTabKey);
             if (isEnabled) {
+                $(document).on("keydown", handleTabKey);
                 $(activeEditor).on("keyEvent", handleEscKey);
             }
         }
 
-        //cm.on('vim-mode-change', handleModeChange);
-    }
-
-    // Toggles vim functionality in the editor.
-    // This function is run by the menu item.
-    function toggleVim(editor) {
-        isEnabled = !isEnabled;
-        CommandManager.get(commandId).setChecked(isEnabled);
-        updateEditorMode();
+        /*cm.on('vim-mode-change', function (event) {
+            console.log(event.mode);
+        });*/
     }
 
     // Define all custom ex commands after the vim module is loaded.
@@ -101,7 +148,7 @@ define(function (require, exports, module) {
                 }, 200);
             } else {
                 CommandManager.execute("file.close");
-                CommandManager.execute("file.addToWorkingSet", {fullPath: rootPath + params.args[0]});
+                CommandManager.execute("file.addToWorkingSet", {fullPath: projectRoot + params.args[0]});
             }
         });
 
@@ -130,7 +177,7 @@ define(function (require, exports, module) {
                 return;
             }
 
-            CommandManager.execute("file.addToWorkingSet", {fullPath: rootPath + relativePath}).done(split);
+            CommandManager.execute("file.addToWorkingSet", {fullPath: projectRoot + relativePath}).done(split);
         });
     });
 
@@ -148,7 +195,7 @@ define(function (require, exports, module) {
     //KeyBindingManager.addBinding("navigate.nextDoc", "Ctrl-w");
 
     $(ProjectManager).on("projectOpen", function (jqEvent, directory) {
-        rootPath = directory._path;
+        projectRoot = directory._path;
 
         // Ensure that at most one file is working when the project opens.
         var workingSet = DocumentManager.getWorkingSet();
