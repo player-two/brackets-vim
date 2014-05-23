7dc768969830d07fc8be77a8920f26aec293fbdb
megalord <jwgriege@sbcglobal.net>
2014-05-22 21:39:12 -0500

    Changed quote style.  Stabalized tab autocomplete and added ./ syntax.

--- README.md
+++ README.md
@@ -16,9 +16,20 @@ Some vim keybindings may interfere with the default Brackets keybindings, in whi
 | Ctrl-Tab   | next document    | none      |
 | Ctrl-W     | close            | next document |
 
+## Splitting
+
+One of the features crucial to my vim usage was splitting the window using the :vsp and :sp commands.  To replicate this in Brackets, the extension manages the Working Files list and the editor CSS.  As such, splitting causes issues when panes are opened in the editor (usually at the bottom of the window by use of an extension).  Rest assured this is a high priority issue, but for now, please unsplit the window before using extensions.
+
 ## File operations
 
-Using normal file operations may break the extension.  Therefore, use only the file commands in the table below instead of opening files from the file tree or menu.
+Using normal file operations (e.g. double clicking a file in the tree) may break the splitting feature of the extension.  Therefore, if using window splitting, use only the file commands in the table below (:e and :vsp) instead of opening files from the file tree or menu.
+
+To encourage and facilitate the use of the vim file commands, the tab autocomplete feature has been replicated.  Press the tab key after typing a partial path to search for a file or directory matching the command argument.  The following syntaxes are supported:
+
+:e [path relative to project root]
+:e ./[path relative to open file]
+
+The first character of the path should not be a slash (/).
 
 ## Commands
 
@@ -33,10 +44,6 @@ Using normal file operations may break the extension.  Therefore, use only the f
 | undo*     | :u        |
 
 *alias for "u" when in normal mode
-
-## Splitting
-
-One of the features crucial to my vim usage was splitting the window using the :vsp and :sp commands.  To replicate this in Brackets, the extension manages the Working Files list and the editor CSS.  As such, splitting causes issues when panes are opened in the editor (usually at the bottom of the window by use of an extension).  Rest assured this is a high priority issue.
 
 ## Future concerns
 
--- main.js
+++ main.js
@@ -1,83 +1,91 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets, window */
 
-/** Simple extension that adds a "File > Hello World" menu item */
 define(function (require, exports, module) {
-    "use strict";
+    'use strict';
 
-    var AppInit         = brackets.getModule("utils/AppInit"),
-        CodeMirror      = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror"),
-        CommandManager  = brackets.getModule("command/CommandManager"),
-        DocumentManager = brackets.getModule("document/DocumentManager"),
-        EditorManager   = brackets.getModule("editor/EditorManager"),
-        ExtensionUtils  = brackets.getModule("utils/ExtensionUtils"),
-        FileSystem      = brackets.getModule("filesystem/FileSystem"),
-        KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
-        Menus           = brackets.getModule("command/Menus"),
-        ProjectManager  = brackets.getModule("project/ProjectManager"),
-        StatusBar       = brackets.getModule("widgets/StatusBar"),
+    var AppInit         = brackets.getModule('utils/AppInit'),
+        CodeMirror      = brackets.getModule('thirdparty/CodeMirror2/lib/codemirror'),
+        CommandManager  = brackets.getModule('command/CommandManager'),
+        DocumentManager = brackets.getModule('document/DocumentManager'),
+        EditorManager   = brackets.getModule('editor/EditorManager'),
+        ExtensionUtils  = brackets.getModule('utils/ExtensionUtils'),
+        FileSystem      = brackets.getModule('filesystem/FileSystem'),
+        KeyBindingManager = brackets.getModule('command/KeyBindingManager'),
+        Menus           = brackets.getModule('command/Menus'),
+        ProjectManager  = brackets.getModule('project/ProjectManager'),
+        StatusBar       = brackets.getModule('widgets/StatusBar'),
 
-        $editorHolder = $("#editor-holder"),
-        $vimModeIndicator = $(document.createElement("div")).text("normal"),
+        $editorHolder = $('#editor-holder'),
+        $vimModeIndicator = $(document.createElement('div')).text('normal'),
 
-        commandId   = "brackets-vim.toggle",
+        commandId   = 'brackets-vim.toggle',
         fileMenu    = Menus.getMenu(Menus.AppMenuBar.FILE_MENU),
         isEnabled   = true,
         isSplit     = false,
-        projectRoot    = "";
+        projectRoot = '';
 
     // Patch the esc key, which does not properly exit insert or visual mode in Brackets.
     // This is likely due to Brackets, since the demo at codemirror.net/demo/vim.html works fine.
     function handleEscKey(jqEvent, editor, event) {
-        if (event.type === "keydown" && event.keyCode === 27) {
+        if (event.type === 'keydown' && event.keyCode === 27) {
             var cm = editor._codeMirror,
                 vimState = cm.state.vim;
 
             if (vimState.insertMode === true) {
-                CodeMirror.keyMap["vim-insert"].Esc(cm);
+                CodeMirror.keyMap['vim-insert'].Esc(cm);
                 //CodeMirror.Vim.handleKey(cm, 'Ctrl-C');
             } else if (vimState.visualMode === true) {
-                CodeMirror.Vim.handleKey(cm, "v");
+                CodeMirror.Vim.handleKey(cm, 'v');
             }
         }
     }
 
     // Extend the vim command dialog to autocomplete filepaths with the tab key.
     function handleTabKey(jqEvent) {
-        var args, dirs, inputElement, partialName, relativePath;
+        var args, dirs, fullPath, inputElement, partialName, relativePath;
         if (event.keyCode === 9) {
             // Check if the vim dialog is focused.
             inputElement = document.activeElement;
-            if (inputElement.parentElement.classList.contains("CodeMirror-dialog-bottom")) {
+            if (inputElement.parentElement.classList.contains('CodeMirror-dialog-bottom')) {
                 // Prevent a focus change to the next input in the DOM.
                 jqEvent.preventDefault();
 
-                args = jqEvent.target.value.split(" ");
-                if (args.length !== 2 || (args[0] !== "e" && args[0] !== "vsp")) {
+                args = jqEvent.target.value.split(' ');
+                if (args.length !== 2 || (args[0] !== 'e' && args[0] !== 'vsp')) {
                     return;
                 }
 
-                dirs = args[1].split("/");
-                relativePath = dirs.slice(0, -1).join("/");
+                dirs = args[1].split('/');
+
+                // The last element of the dirs array is (possibly) not an actual directory.
+                partialName = dirs.pop();
+                relativePath = dirs.join('/');
+
+                // Ensure the last character of the path is a slash.
                 if (relativePath.length > 0) {
-                    relativePath += "/";
+                    relativePath += '/';
                 }
-                partialName = dirs.slice(-1).join("/");
+
+                // Check to see if the path is relative to the current file (instead of the project root).
+                // This is denoted with the './' prefix.
+                if (relativePath.slice(0, 2) === './') {
+                    relativePath = EditorManager.getActiveEditor().document.file._parentPath.slice(projectRoot.length);
+                }
 
                 FileSystem.resolve(projectRoot + relativePath, function (notUsed, Directory) {
-                    var match = "";
+                    var match = '';
                     Directory._contents.forEach(function (item) {
-                        if (item._name.indexOf(partialName) !== -1) {
+                        if (item._name.indexOf(partialName) === 0) {
                             match = item._name;
-                        }
-
-                        if (item.hasOwnProperty("_isDirectory") && item._isDirectory) {
-                            match += "/";
+                            if (item.hasOwnProperty('_isDirectory') && item._isDirectory) {
+                                match += '/';
+                            }
                         }
                     });
 
                     if (match.length > 0) {
-                        inputElement.value = args[0] + " " + relativePath + match;
+                        inputElement.value = args[0] + ' ' + relativePath + match;
                     }
                 });
             }
@@ -91,7 +99,7 @@ define(function (require, exports, module) {
     // Splits the UI so that two documents may be viewed at the same time.
     function split() {
         isSplit = true;
-        $editorHolder.addClass("split-view");
+        $editorHolder.addClass('split-view');
     }
 
     // Toggles vim functionality in the editor.
@@ -100,9 +108,9 @@ define(function (require, exports, module) {
         isEnabled = !isEnabled;
         CommandManager.get(commandId).setChecked(isEnabled);
         if (isEnabled) {
-            $(document).on("keydown", handleTabKey);
+            $(document).on('keydown', handleTabKey);
         } else {
-            $(document).off("keydown", handleTabKey);
+            $(document).off('keydown', handleTabKey);
         }
         updateEditorMode();
     }
@@ -110,7 +118,7 @@ define(function (require, exports, module) {
     // Revert the UI back to a single-document view.
     function unsplit() {
         isSplit = false;
-        $editorHolder.removeClass("split-view");
+        $editorHolder.removeClass('split-view');
     }
 
     // Update the active editor with the vim mode.
@@ -124,48 +132,48 @@ define(function (require, exports, module) {
 
         cm = activeEditor._codeMirror;
 
-        if (isEnabled !== cm.getOption("vimMode")) {
-            cm.setOption("vimMode", isEnabled);
+        if (isEnabled !== cm.getOption('vimMode')) {
+            cm.setOption('vimMode', isEnabled);
 
             if (isEnabled) {
-                cm.on("vim-mode-change", handleVimModeChange);
-                $(activeEditor).on("keyEvent", handleEscKey);
+                cm.on('vim-mode-change', handleVimModeChange);
+                $(activeEditor).on('keyEvent', handleEscKey);
             } else {
-                cm.off("vim-mode-change", handleVimModeChange);
-                $(activeEditor).off("keyEvent", handleEscKey);
+                cm.off('vim-mode-change', handleVimModeChange);
+                $(activeEditor).off('keyEvent', handleEscKey);
             }
         }
     }
 
     // Define all custom ex commands after the vim module is loaded.
-    brackets.getModule(["thirdparty/CodeMirror2/keymap/vim"], function () {
-        CodeMirror.Vim.defineEx("edit", "e", function (cm, params) {
+    brackets.getModule(['thirdparty/CodeMirror2/keymap/vim'], function () {
+        CodeMirror.Vim.defineEx('edit', 'e', function (cm, params) {
             if (!params.hasOwnProperty('args')) {
                 // The timeout is used so that the enter key can be released before the quick open dialog is shown,
                 // preventing the first file in the dialog list from being immediately opened.
                 setTimeout(function () {
-                    CommandManager.execute("file.close");
-                    CommandManager.execute("navigate.quickOpen");
+                    CommandManager.execute('file.close');
+                    CommandManager.execute('navigate.quickOpen');
                 }, 200);
             } else {
-                CommandManager.execute("file.close");
-                CommandManager.execute("file.addToWorkingSet", {fullPath: projectRoot + params.args[0]});
+                CommandManager.execute('file.close');
+                CommandManager.execute('file.addToWorkingSet', {fullPath: projectRoot + params.args[0]});
             }
         });
 
-        CodeMirror.Vim.defineEx("quit", "q", function () {
-            CommandManager.execute("file.close");
+        CodeMirror.Vim.defineEx('quit', 'q', function () {
+            CommandManager.execute('file.close');
 
             if (isSplit) {
                 unsplit();
             }
         });
 
-        CodeMirror.Vim.defineEx("write", "w", function () {
-            CommandManager.execute("file.save");
+        CodeMirror.Vim.defineEx('write', 'w', function () {
+            CommandManager.execute('file.save');
         });
 
-        CodeMirror.Vim.defineEx("vsplit", "vsp", function (cm, params) {
+        CodeMirror.Vim.defineEx('vsplit', 'vsp', function (cm, params) {
             // Do nothing if already split.
             if (isSplit) {
                 return;
@@ -178,29 +186,29 @@ define(function (require, exports, module) {
                 return;
             }
 
-            CommandManager.execute("file.addToWorkingSet", {fullPath: projectRoot + relativePath}).done(split);
+            CommandManager.execute('file.addToWorkingSet', {fullPath: projectRoot + relativePath}).done(split);
         });
     });
 
     // Register the enable command.
-    CommandManager.register("Enable Vim", commandId, toggleVim);
+    CommandManager.register('Enable Vim', commandId, toggleVim);
     CommandManager.get(commandId).setChecked(isEnabled);
     if (isEnabled) {
-        $(document).on("keydown", handleTabKey);
+        $(document).on('keydown', handleTabKey);
     }
 
     // Add the enable command to the file menu.
     fileMenu.addMenuDivider();
     fileMenu.addMenuItem(commandId);
 
-    ExtensionUtils.loadStyleSheet(module, "main.css");
+    ExtensionUtils.loadStyleSheet(module, 'main.css');
 
-    StatusBar.addIndicator("vim-mode", $vimModeIndicator, true);
+    StatusBar.addIndicator('vim-mode', $vimModeIndicator, true);
 
     // Doesn't work yet.
-    //KeyBindingManager.addBinding("navigate.nextDoc", "Ctrl-w");
+    //KeyBindingManager.addBinding('navigate.nextDoc', 'Ctrl-w');
 
-    $(ProjectManager).on("projectOpen", function (jqEvent, directory) {
+    $(ProjectManager).on('projectOpen', function (jqEvent, directory) {
         projectRoot = directory._path;
 
         // Ensure that at most one file is working when the project opens.
@@ -210,7 +218,7 @@ define(function (require, exports, module) {
         }
     });
 
-    $(DocumentManager).on("currentDocumentChange", function () {
+    $(DocumentManager).on('currentDocumentChange', function () {
         updateEditorMode();
     });
 });
